fd7cf43dab7f00c1d5e996979de091eba03f3e2c
hochan <hochan9@gmail.com>
2024-06-27 17:42:29 +0900

    fix: db 변경

--- src/test/java/com/sparta/filterpattern/FilterPatternApplicationTests.java
+++ src/test/java/com/sparta/filterpattern/FilterPatternApplicationTests.java
@@ -2,8 +2,10 @@ package com.sparta.filterpattern;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FilterPatternApplicationTests {
 
     @Test
