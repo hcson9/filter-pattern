c272e15beecaeb42bda81b1b2bbef3a930d4d91a
hochan <hochan9@gmail.com>
2024-07-02 14:24:07 +0900

    fix: circle ci test

---
+++ .circleci/deploy.sh
@@ -0,0 +1,13 @@
+CONTAINER_ID=$(sudo docker ps -q --filter "publish=8080-8080")
+
+if [ ! -z "$CONTAINER_ID" ]; then
+  sudo docker stop $CONTAINER_ID
+  sudo docker rm $CONTAINER_ID
+fi
+
+sudo docker pull $DOCKERHUB_USERNAME/$DOCKER_IMAGE_TAG_NAME:latest
+sudo docker run $DOCKERHUB_USERNAME/$DOCKER_IMAGE_TAG_NAME:latest -d -p 8080:8080 \
+    -e DB_USERNAME=$DB_USERNAME \
+    -e DB_PASSWORD=$DB_PASSWORD \
+    -e DB_URL=$DB_URL
+    filter-pattern
